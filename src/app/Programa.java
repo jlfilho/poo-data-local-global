585231f328999aab0bd15aae78fd4b66c86dd411
João da Mata <jlfilho@uea.edu.br>
2022-08-24 03:04:39 -0400

    Obter dados de uma data-hora local

--- src/app/Programa.java
+++ src/app/Programa.java
@@ -9,8 +9,6 @@ public class Programa {
 	public static void main(String[] args) {
 
 		// Converter data-hora global para local
-		// LocalDateTime d01 = LocalDateTime.parse("2022-08-23T01:30:06");
-
 		Instant d01 = Instant.parse("2022-08-23T01:30:06Z");
 		System.out.println("Horário Londres: "+d01);
 		LocalDateTime r1 = LocalDateTime.ofInstant(d01, ZoneId.systemDefault());
@@ -18,6 +16,14 @@ public class Programa {
 
 		LocalDateTime r2 = LocalDateTime.ofInstant(d01, ZoneId.of("America/Sao_Paulo"));
 		System.out.println("Horário SP: "+r2);
+		
+		//Obter dados de uma data-hora local
+		LocalDateTime d02 = LocalDateTime.parse("2022-08-23T01:30:06");
+		System.out.println(d02.getDayOfMonth());
+		System.out.println(d02.getMonth());
+		System.out.println(d02.getYear());
+		System.out.println(d02.getHour());
+		System.out.println(d02.getMinute());
 
 		/*
 		 * for (String s : ZoneId.getAvailableZoneIds()) {
